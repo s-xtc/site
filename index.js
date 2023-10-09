17e268ab66225cc48910e5119c416657b4d97f1c
s-xtc <36300235+s-xtc@users.noreply.github.com>
2023-10-09 21:12:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const server = require('server');
 const { get, socket } = server.router;
 const { render } = server.reply;
 
+// Update everyone with the current user count
 const updateCounter = ctx => {
-  ctx.io.emit('count', ctx.io.sockets.sockets.length);
+  ctx.io.emit('count', Object.keys(ctx.io.sockets.sockets).length);
+};
+
+// Send the new message to everyone
+const sendMessage = ctx => {
+  ctx.io.emit('message', ctx.data);
 };
 
 server([
-  // For the initial load render the index.html
   get('/', ctx => render('index.html')),
-
-  // Join/leave the room
   socket('connect', updateCounter),
-  socket('disconnect', updateCounter)
+  socket('disconnect', updateCounter),
+  socket('message', sendMessage)
 ]);
